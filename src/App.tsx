bff13d5b954c2ec33ba25a74eaa765eef432e98a
guilherme1s <2000guilherme.silva@gmail.com>
2026-02-21 11:50:29 -0300

    [add] Setup inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./index.css";
+import { Button } from "@/components/ui/button";
+
+export function App() {
+  return (
+    <div className="flex h-screen min-h-svh flex-col items-center justify-center">
+      <Button>enviar</Button>
+    </div>
+  );
+}
