004cbe3e835ecb7800b534d724c2427837846456
guilherme1s <2000guilherme.silva@gmail.com>
2026-02-22 10:55:28 -0300

    [ADD] Cria página de login

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Dashboard } from "./pages/app/dashboard";
-import { SignIn } from "./pages/auth/singin";
+import { SignIn } from "./pages/auth/sing-in";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 
