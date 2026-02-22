337366170ddc88d9809673a61f6268da8c2b5dc4
guilherme1s <2000guilherme.silva@gmail.com>
2026-02-22 12:01:51 -0300

    [ADD] Cria página de sign-up

--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -5,6 +5,7 @@ import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
+import { Link } from "react-router-dom";
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -40,6 +41,14 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button
+          variant="ghost"
+          asChild
+          className="absolute top-8 right-8"
+        >
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
