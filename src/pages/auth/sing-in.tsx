d94475550e1d83e8d76fa0632b7b16a8e102914d
guilherme1s <2000guilherme.silva@gmail.com>
2026-02-22 11:08:37 -0300

    [ADD] adiciona validação do formulário

--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -2,8 +2,28 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+type SignInForm = z.infer<typeof signInForm>;
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
+
+  async function handleSignIn(data: SignInForm) {
+    console.log(data);
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -18,13 +38,15 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Seu email</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register("email")} />
             </div>
 
-            <Button type="submit" className="w-full">Acessar painel</Button>
+            <Button type="submit" className="w-full cursor-pointer" disabled={isSubmitting}>
+              Acessar painel
+            </Button>
           </form>
         </div>
       </div>
