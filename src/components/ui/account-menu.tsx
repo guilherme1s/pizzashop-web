b177b13c799e583707fc93abd51778012e88da7b
guilherme1s <2000guilherme.silva@gmail.com>
2026-02-23 20:12:00 -0300

    [ADD] Cria header do projeto

---
+++ src/components/ui/account-menu.tsx
@@ -0,0 +1,46 @@
+import { Building, ChevronDown, LogOut } from "lucide-react";
+import { Button } from "./button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./dropdown-menu";
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex items-center gap-2 select-none"
+        >
+          Pizza Shop
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Guilherme Silva Evangelista</span>
+          <span className="text-muted-foreground text-xs font-normal">
+            2000guilherme.silva@gmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem>
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -50,7 +50,7 @@ export function SignUp() {
           <Link to="/sign-in">Fazer login</Link>
         </Button>
 
-        <div className="flex w-[350px] flex-col justify-center gap-6">
+        <div className="flex w-\[350px\] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
               Criar conta grátis
