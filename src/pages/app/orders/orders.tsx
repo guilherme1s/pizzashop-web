c3ff84fd0c4ba5598c23afafa8feee1bc859c813
guilherme1s <2000guilherme.silva@gmail.com>
2026-02-28 09:41:37 -0300

    [ADD] Adiciona componente de paginação

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -8,6 +8,7 @@ import {
 import { Helmet } from "react-helmet-async";
 import { OrderTableRow } from "./order-table-row";
 import { OrderTableFilters } from "./order-table-filters";
+import { Pagination } from "@/components/ui/pagination";
 
 export function Orders() {
   return (
@@ -15,32 +16,34 @@ export function Orders() {
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
 
-      <div className="space-y-2.5">
-        <OrderTableFilters />
+        <div className="space-y-2.5">
+          <OrderTableFilters />
 
-        <div className="rouded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-16"></TableHead>
-                <TableHead className="w-35">Identificador</TableHead>
-                <TableHead className="w-45">Realizado há</TableHead>
-                <TableHead className="w-35">Status</TableHead>
-                <TableHead>Cliente</TableHead>
-                <TableHead className="w-35">Total do pedido</TableHead>
-                <TableHead className="w-41"></TableHead>
-                <TableHead className="w-33"></TableHead>
-              </TableRow>
-            </TableHeader>
+          <div className="rouded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-16"></TableHead>
+                  <TableHead className="w-35">Identificador</TableHead>
+                  <TableHead className="w-45">Realizado há</TableHead>
+                  <TableHead className="w-35">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-35">Total do pedido</TableHead>
+                  <TableHead className="w-41"></TableHead>
+                  <TableHead className="w-33"></TableHead>
+                </TableRow>
+              </TableHeader>
 
-            <TableBody>
-              {Array.from({ length: 15 }).map((_, i) => {
-                return <OrderTableRow key={i} />;
-              })}
-            </TableBody>
-          </Table>
+              <TableBody>
+                {Array.from({ length: 15 }).map((_, i) => {
+                  return <OrderTableRow key={i} />;
+                })}
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
