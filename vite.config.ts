e1bfe6932dfad4ec07d166d99dcc0e3695de3802
guilherme1s <2000guilherme.silva@gmail.com>
2026-03-17 20:52:04 -0300

    [ADD] Configurações pra realizar testes unitários

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import path from "node:path"
-import tailwindcss from "@tailwindcss/vite"
+import path from 'node:path'
 
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+import type { InlineConfig } from 'vitest/node'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	test: {
+		globals: true,
+		setupFiles: ['./test/setup.ts'],
+		environment: "happy-dom",
+	},
+} as UserConfig & {
+	test: InlineConfig
 })
