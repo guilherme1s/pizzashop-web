9227942644d82cd18f37ba7c20227ae313fdec2f
guilherme1s <2000guilherme.silva@gmail.com>
2026-03-20 19:03:10 -0300

    [FIX] Correção no tailwind do arquivo vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
 import type { InlineConfig } from 'vitest/node'
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
